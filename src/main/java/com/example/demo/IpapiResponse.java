b634f2e95076cf61d2737710daf45ddb0cdab5bf
Naresh-Kushwaha <136280624+Naresh-Kushwaha@users.noreply.github.com>
2025-01-20 06:19:18 +0530

    responseUpdated

---
+++ src/main/java/com/example/demo/IpapiResponse.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class IpapiResponse {
+    private String city;
+    private String region_name;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRegion_name() {
+        return region_name;
+    }
+
+    public void setRegion_name(String region_name) {
+        this.region_name = region_name;
+    }
+}
--- src/main/java/com/example/demo/WeatherController.java
+++ src/main/java/com/example/demo/WeatherController.java
@@ -7,10 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 @CrossOrigin("*")
 @RestController
@@ -36,8 +32,9 @@ public class WeatherController {
             ipAddress = request.getRemoteAddr();
         }
         String url=String.format("%s/%s?access_key=%s",ipapiurl,ipAddress,ipapikey);
-          WeatherResponse response=restTemplate.getForObject(url, WeatherResponse.class);
-        return searchWeather(response.getCity());
+          IpapiResponse response=restTemplate.getForObject(url, IpapiResponse.class);
+        assert response != null;
+        return searchWeather(response.getRegion_name());
 
 
     }
@@ -45,16 +42,7 @@ public class WeatherController {
     public String searchWeather(@RequestParam String city) throws IOException, InterruptedException {
         String url=String.format("https://api.openweathermap.org/data/2.5/weather?units=metric&q=%s&appid=4c3fb3dc336e3f3a2ccfc656bf857318",city);
         return restTemplate.getForObject(url, String.class);
-//              String url=String.format("%s/current?place=%s&units=matric&lang=en&mode=json",weatherurl,city);
-//        HttpRequest request = HttpRequest.newBuilder()
-//                .uri(URI.create(url))
-//                .header("x-rapidapi-key", weatherkey)
-//                .header("x-rapidapi-host", "weather-api167.p.rapidapi.com")
-//                .header("Accept", "application/json")
-//                .method("GET", HttpRequest.BodyPublishers.noBody())
-//                .build();
-//        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-//        return  response.body();
+
 
     }
 
